13c637fcf6e2f8a32ff18bd357b68111455585db
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2023-01-24 09:50:15 +0330

    using cors

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const port = 3000;
 
 const app = express();
 
+var cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
