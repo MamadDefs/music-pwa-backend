b1806e449b8b4052ab4b31353f54e739c980af4e
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2023-02-03 12:28:57 +0330

    adding user's information to the api/users/auth

--- routeHandlers/userRouteHandler.js
+++ routeHandlers/userRouteHandler.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const jsmediatags = require('jsmediatags');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const User = require('../models/userModel');
 const Music = require('../models/musicModel');
 const sendMail = require('../utilities/sendMail');
@@ -24,14 +24,14 @@ awaitableJsmediatags = (filename) => {
 }
 
 exports.signUpSubmission = async (req, res, next) => {
-    try{
+    try {
         const user = await User.create({
             username: req.body.username,
             email: req.body.email,
             password: req.body.password,
             passwordConfirm: req.body.passwordConfirm
         });
-        const token = jwt.sign({id: user._id, username: user.username}, secret, {
+        const token = jwt.sign({ id: user._id, username: user.username }, secret, {
             expiresIn: '90d'
         });
         // res.cookie('jwtToken', token, {
@@ -44,7 +44,7 @@ exports.signUpSubmission = async (req, res, next) => {
             token
         });
     }
-    catch(err){
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
@@ -55,37 +55,37 @@ exports.activateEmail = async (req, res, next) => {
         const user = await User.findOneAndUpdate({
             activationToken: hashedToken
         },
-        {
-            isActivated: true,
-            activationToken: null
-        }, {runValidators: true, new: true});
-        if(!user) return next(new MyError('User not found or already activated.', 400));
+            {
+                isActivated: true,
+                activationToken: null
+            }, { runValidators: true, new: true });
+        if (!user) return next(new MyError('User not found or already activated.', 400));
 
         res.status(200).json({
             user
         });
-    } 
+    }
     catch (err) {
         next(new MyError(err, 500));
     }
 }
 
 exports.signInSubmission = async (req, res, next) => {
-    try{
+    try {
         const user = await User.findOne({
             username: req.body.username
         }).select('+password');
-        if(!user || !await bcrypt.compare(req.body.password, user.password))
+        if (!user || !await bcrypt.compare(req.body.password, user.password))
             return next(new MyError('There is no user with this username or password is wrong.', 401));
 
-        const token = jwt.sign({id: user._id, username: user.username}, secret, {
+        const token = jwt.sign({ id: user._id, username: user.username }, secret, {
             expiresIn: '90d'
         });
         // res.cookie('jwtToken', token, {
         //     maxAge: 90 * 24 * 60 * 60 * 1000,
         //     httpOnly: true
         // });
-        
+
         let userRole = user.role === 'admin' ? 'admin' : (user.role === 'user' ? 'user' : undefined);
 
         res.status(200).json({
@@ -93,7 +93,7 @@ exports.signInSubmission = async (req, res, next) => {
             token
         });
     }
-    catch(err){
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
@@ -101,17 +101,17 @@ exports.signInSubmission = async (req, res, next) => {
 exports.auth = async (req, res, next) => {
     try {
         const user = await getLogedInUser(req.body.jwtToken);
-        if(user)
+        if (user)
             res.status(200).json({
                 isLogin: true,
-                role: user.role
+                user
             });
         else
             res.status(200).json({
-                isLogin: false 
+                isLogin: false
             });
     }
-    catch(err) {
+    catch (err) {
         res.status(403);
     }
 }
@@ -133,11 +133,11 @@ exports.resetPasswordGetEmail = (req, res, next) => {
 
 exports.resetPasswordSendEmail = async (req, res, next) => {
     try {
-        const user = await User.findOne({email: req.body.email});
-        if(!user) return next(new MyError('User not found', 404));
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return next(new MyError('User not found', 404));
 
         const resetToken = user.createResetToken();
-        await user.save({validateBeforeSave: false});
+        await user.save({ validateBeforeSave: false });
 
         const resetURL = `http://127.0.0.1:3000/users/reset-password/${resetToken}`;
         const msg = `Please click the following URL to reset your password. (This URL will expire after 15 minutes!!!)\nIf you don't request for reseting password ignore this Email.\n${resetURL}`;
@@ -167,14 +167,14 @@ exports.resetPasswordForm = (req, res, next) => {
 }
 
 exports.resetPasswordSubmission = async (req, res, next) => {
-    try{
+    try {
         const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
-    
+
         const user = await User.findOne({
             resetPasswordToken: hashedToken,
-            resetPasswordExpiredTime: {$gt: Date.now()}
+            resetPasswordExpiredTime: { $gt: Date.now() }
         });
-        if(!user) return next(new MyError('The token is Invalid OR has been Expired.', 401));
+        if (!user) return next(new MyError('The token is Invalid OR has been Expired.', 401));
 
         user.password = req.body.password;
         user.passwordConfirm = req.body.passwordConfirm;
@@ -182,7 +182,7 @@ exports.resetPasswordSubmission = async (req, res, next) => {
         user.resetPasswordExpiredTime = undefined;
         await user.save();
 
-        const token = jwt.sign({id: user._id, username: user.username}, secret, {
+        const token = jwt.sign({ id: user._id, username: user.username }, secret, {
             expiresIn: '90d'
         });
         res.cookie('jwtToken', token, {
@@ -194,18 +194,18 @@ exports.resetPasswordSubmission = async (req, res, next) => {
             message: 'Password changed successfully.'
         });
     }
-    catch(err){
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
 
 exports.userProfile = async (req, res, next) => {
-    try{
+    try {
         res.status(200).json({
             user: req.user
         });
     }
-    catch(err){
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
@@ -213,27 +213,27 @@ exports.userProfile = async (req, res, next) => {
 exports.uploadProfileImage = async (req, res, next) => {
     try {
         const user = req.user;
-        if(!user) return next(new MyError('Please log in', 403));
+        if (!user) return next(new MyError('Please log in', 403));
 
-        if(!req.files){
+        if (!req.files) {
             return next(new MyError('Please upload an image', 400));
         }
         const img = req.files.profileImage;
         const dir = __dirname + '/../public/uploads/profile_images/' + img.name;
         console.log(img.mimetype);
-        if(!img.mimetype.match(/image/g))
+        if (!img.mimetype.match(/image/g))
             return next(new MyError('Please upload an image file'), 400);
 
         await img.mv(dir);
 
         await user.updateOne({
             profileImage: dir
-        }, {runValidators: true, new: true});
+        }, { runValidators: true, new: true });
 
         res.status(200).json({
             user
         });
-    } 
+    }
     catch (err) {
         next(new MyError(err, 500));
     }
@@ -241,35 +241,35 @@ exports.uploadProfileImage = async (req, res, next) => {
 
 exports.adminPanel = async (req, res, next) => {
     try {
-        
+
     }
-    catch(err) {
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
 
 exports.adminUploadMusic = async (req, res, next) => {
     try {
-        if(req.user.role !== 'admin') return next(new MyError('access denied.', 403));
+        if (req.user.role !== 'admin') return next(new MyError('access denied.', 403));
         const artists = req.body.artist.split(',');
         artists.forEach((el, index, arr) => {
             arr[index] = el.trim();
         });
-        
+
         const categories = req.body.category.split(',');
         categories.forEach((el, index, arr) => {
             arr[index] = el.trim();
         });
 
-        if(!req.files){
+        if (!req.files) {
             return next(new MyError('Please upload a music', 400));
         }
         const musicFile = req.files.music;
         const dir = __dirname + '/../public/uploads/musics/' + musicFile.name;
         console.log(musicFile.mimetype);
-        if(!musicFile.mimetype.match(/audio/g))
+        if (!musicFile.mimetype.match(/audio/g))
             return next(new MyError('Please upload a audio file'), 400);
-        
+
         await musicFile.mv(dir);
         // const tags = await awaitableJsmediatags(dir);
         // console.log(tags);
@@ -285,7 +285,7 @@ exports.adminUploadMusic = async (req, res, next) => {
             music
         });
     }
-    catch(err) {
+    catch (err) {
         next(new MyError(err, 500));
     }
 }
